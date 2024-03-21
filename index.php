33f56a4436a27285f082714ed681eb6a64656b87
berrymubarak <48374402+berrymubarak@users.noreply.github.com>
2024-03-21 23:13:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-   <meta http-equiv="refresh" content="5; URL=https://www.tutorialspoint.com/index.htm" />
+   <meta http-equiv="refresh" content="5; URL=https://edunetter.com/index.php" />
 </head>
 <body>
-   <p>If you are not redirected in five seconds, <a href="https://www.tutorialspoint.com/index.htm">click here</a>.</p>
+   <p>If you are not redirected in five seconds, <a href="https://edunetter.com/index.php">click here</a>.</p>
 </body>
 </html>
